bc44cdd75e3c0371ad571df300c13b93c7833636
Abonyo Mitchell Nina <157416946+ninamitchell23@users.noreply.github.com>
2024-12-18 19:49:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number=2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'a';// enclose a character in single quotes
+            string firstName = "Mitchell"; // enclose a string in double quotes
+            //Using var
+            var Number = 1;
+            var Count = 10;
+            var Totalprice = 20.95f;
+            var Character = 'a';
+            Console.WriteLine(character);
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(firstName);
+            Console.WriteLine(Number);
+            Console.WriteLine(Count);
+            Console.WriteLine(Totalprice);
+            Console.WriteLine(Character);
+            
+        }
+    }
+}
